9b9a14e51bc5f01ca25c37eff4a768b8bd106b27
Pâris DOUADY <paris.douady@hotmail.fr>
2024-11-19 12:01:03 +0100

    workspace

--- playerone-sdk-sys/src/lib.rs
+++ playerone-sdk-sys/src/lib.rs
@@ -1,4 +1,4 @@
-#[allow(non_snake_case, non_upper_case_globals)]
+#[allow(non_snake_case, non_upper_case_globals, non_camel_case_types)]
 mod bindings {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
